0177ba8ebf454c89f35466b10c1a42bfa93a21da
derciotorres <dercio.torres14@gmail.com>
2018-04-02 07:37:41 -0400

    Final

--- mult.php
+++ mult.php
@@ -1,28 +1,56 @@
 <?php
-$size = mt_rand(5, 10);
-$table = "<table>\n";
-for ($rows= 1; $rows <= $size; $rows++)
-{
-	$table .="\t<tr>";
-	for($cols =1; $cols<=$size; $cols++):
-	$table .="<td>" . $rows*$cols . "</td>";
-	endfor;
-	$table .="\t</tr>\n";
-	
-}
-$table .= "</table>\n";
-echo $table;
-?>
 
-<!Doctype html>
+$table='';
+for ($rows=0; $rows<10; $rows++)
+	{
+    $table .= "<tr>";
+    for ($cols=0; $cols<10; $cols++) 
+	{
+        $colors = randColor();
+        $table .= "<td style='background-color:#$colors'>$colors<br><span class='whitetext'>$colors</span></td>";
+    }
+    $table .= '</tr>';
+	}
+	
+	
+function randColor() 
+{	
+
+	$blue = rand(0,255);
+    $red = rand(0,255);
+    $green = rand(0,255);
+    
+
+    return sprintf('%02X%02X%02X', $red, $green, $blue);
+}
+?>
+<!doctype html>
+
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Multiplication table</title>
+<style type='text/css'>
+table {
+    border-collapse: separate;
+	
+}
+td {
+    width: 50;
+    height: 50;
+    font-family: Arial;
+    font-size: 10pt;
+    text-align: center;
+}
+.whitetext {
+    color: white;
+}
 
+</style>
+<title>Random grid</title>
 </head>
 <body>
-<?php echo $table; ?>
+<table align="center">
+    <?=$table?>
+</table>
 </body>
-
 </html>
